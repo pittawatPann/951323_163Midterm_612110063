d68630cfc2eb639b4167f7fbb2d8ba225c8efac8
DESKTOP-325D3AA\Pittawat <pittawat_pan@cmu.ac.th>
2020-10-03 15:43:10 +0700

    Update Progress #6 Random obstacle (Completed)

---
+++ 951323_163_Midterm_612110063/Assets/_Script/SpawnObstacle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacle : MonoBehaviour
+{
+    public GameObject[] Prefabs;
+    public GameObject[] Spawnpoint;
+    
+    [SerializeField]
+    private float TimeBetweenwave =1f;
+    [SerializeField]
+    private float TimeTospawn = 2f;
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= TimeTospawn)
+        {
+                SpawnBlock();
+             TimeTospawn = Time.time + TimeBetweenwave;
+        }
+        
+        
+    }
+    void SpawnBlock()
+    {
+        int randomIndex = Random.Range(0, Spawnpoint.Length);
+        int randomball = Random.Range(0, Prefabs.Length);
+        for(int i = 0; i< Spawnpoint.Length; i++)
+        {
+            if(randomIndex != i)
+            {
+                Instantiate(Prefabs[randomball], Spawnpoint[i].transform.position, Quaternion.identity);
+            }
+        }
+        
+    }
+}
